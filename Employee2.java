bd3dfaccc0a79f1f62f860d44d9af38cd934189b
Hasmawati <asmaif08@gmail.com>
2018-08-28 15:26:36 +0700

    first commit

---
+++ Employee2.java
@@ -0,0 +1,38 @@
+public class Employee2{
+    String nama;
+    int usia;
+    String loker;
+    double gaji;
+    
+public Employee(String nama){
+    this.nama = nama;
+}
+
+public void setUsia(int age){
+    usia = age;
+}
+
+public void setLoker(String pekerjaan){
+    loker = pekerjaan;
+}
+
+public void setGaji(double salary){
+    gaji = salary;
+}
+
+public void tampilkanEmployee(){
+    System.out.println ("Nama Pegawai : "+nama);
+    System.out.println ("Umur   : "+usia);
+    System.out.println ("Jenis Pekerjaan : "+loker);
+    System.out.println ("Jumlah Gaji : "+gaji);
+}
+
+public static void main (String args[]){
+    Employee pegawai1 = new Employee ("Hasma");
+    pegawai1.setUsia(28);
+    pegawai1.setLoker("Dosen");
+    pegawai1.setGaji(5000000);
+    pegawai1.tampilkanEmployee();
+    
+}
+}
