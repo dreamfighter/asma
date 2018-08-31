f1d29a56cf9f47be28710dee30c62eca8fb40fd3
Hasmawati <asmaif08@gmail.com>
2018-08-31 09:15:02 +0700

    bikin objek

--- Employee3.java
+++ Employee3.java
@@ -1,27 +1,35 @@
 import java.io.*;
 import java.util.*;
 public class Employee3{
-    String nama;
-    int umur;
-    String loker;
-    double gaji;
+    private String nama;
+    private int umur;
+    private String loker;
+    private double gaji;
     
-public Employee3(String nama){
-    this.nama = nama;
+public void setNama(String nama){
+	this.nama = nama;
+}	
+public String getNama(){
+	return nama;
 }
-
-public void setUsia(int age){
-    umur = age;
+public void setUsia(int umur){
+    this.umur = umur;
 }
-
-public void setLoker(String pekerjaan){
-    loker = pekerjaan;
+public int getUsia(){
+	return usia;
 }
-
-public void setGaji(double salary){
-    gaji = salary;
+public void setLoker(String loker){
+    this.loker = loker;
 }
-
+public String getLoker(){
+	return loker;
+}
+public void setGaji(double gaji){
+    this.gaji = gaji;
+}
+public double getGaji(){
+	return gaji;
+}
 public void tampilkanEmployee(){
     System.out.println ("Nama Pegawai : "+nama);
     System.out.println ("Umur   : "+umur);
@@ -37,10 +45,10 @@ public static void main (String args[])throws IOException{
 	
 	Scanner sc = new Scanner(System.in);	
 	System.out.print ("Masukan nama pegawai : ");
-	String nama = sc.nextLine();
+	String name = sc.nextLine();
 	
 	System.out.print ("Masukan umur pegawai : ");
-	int umur = sc.nextInt();
+	int usia = sc.nextInt();
 	
 	System.out.print ("Masukan Gaji pegawai : ");
 	Float gaji = sc.nextFloat();
@@ -53,11 +61,18 @@ public static void main (String args[])throws IOException{
 	System.out.println ("Data Pegawai : ");
 	System.out.println ("==========");	
 	
-	//Employee3 pegawai = new Employee3("");
-	System.out.println ("Nama pegawai: "+nama);
-	System.out.println ("Umur pegawai: "+umur);
-	System.out.println ("Jenis Pekerjaan: "+pekerjaan);
-	System.out.println ("Gaji: "+gaji);
+	Employee3 pegawai = new Employee3();
+	pegawai.setNama(name);
+	pegawai.setUsia(sc.nextInt);
+	pegawai.setLoker(pekerjaan);
+	pegawai.setGaji(gaji);
+	
+	//pegawai.tampilkanEmployee();
+	
+	System.out.println ("Nama pegawai: "+pegawai.getNama());
+	System.out.println ("Umur pegawai: "+pegawai.getUsia());
+	System.out.println ("Jenis Pekerjaan: "+pegawai.getLoker());
+	System.out.println ("Gaji: "+pegawai.getGaji());
 	
 	//pegawai.tampilkanEmployee();
     // } catch (IOException e){
